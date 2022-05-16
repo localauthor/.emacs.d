baf621768c6865cb5a333552f649f29eca6bc157
Grant Rosson <grantrosson@gmail.com>
2022-05-16 10:51:14 +0300

    Use rga, ripgrep-all, for consult-ripgrep-devonthink

--- my-lisp/devonthink-dir.el
+++ my-lisp/devonthink-dir.el
@@ -21,12 +21,12 @@
       (consult-find devonthink-dir initial))))
 
 ;; DevonThink Links
-;; obsolute, use org-link-set-parameters instead
-(org-add-link-type "x-devonthink-item" 'org-devonthink-item-open)
+;; obsolete, use org-link-set-parameters instead
+;; (org-add-link-type "x-devonthink-item" 'org-devonthink-item-open)
 
-(defun org-devonthink-item-open (uid)
-  "Open the given UID, which is a reference to an item in Devonthink."
-  (shell-command (concat "open \"x-devonthink-item:" uid "\"")))
+;; (defun org-devonthink-item-open (uid)
+;;   "Open the given UID, which is a reference to an item in Devonthink."
+;;   (shell-command (concat "open \"x-devonthink-item:" uid "\"")))
 
 ;; (defun gr/devonthink-find-file (keys-entries)
 ;;   "Search devonthink archive for file, using `consult-find'."
@@ -36,6 +36,15 @@
 ;;       'yank
 ;;     (define-key embark-file-map (kbd "RET") 'find-file)
 ;;     (consult-find gr/devonthink-dir)))
+
+
+(defun consult-ripgrep-devonthink ()
+  (interactive)
+  (let ((consult-ripgrep-args "rga --null --line-buffered --color=never --max-columns=1000 --path-separator /   --smart-case --no-heading --line-number .")
+        (vertico-count 15)
+        (consult-preview-key nil))
+    (consult-ripgrep gr/pdf-directory)))
+
 
 (provide 'devonthink-dir)
 ;;; dir-devonthink.el ends here
