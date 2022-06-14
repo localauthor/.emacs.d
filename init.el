1d04f58b5fb0413e5eb2e0fbea6e25e5a258e12d
Grant Rosson <grantrosson@gmail.com>
2022-06-14 23:36:04 +0300

    Add error-handling to straight-fetch-report

--- init.el
+++ init.el
@@ -119,41 +119,63 @@
   (with-current-buffer (get-buffer-create "*straight-fetch-report*")
     (read-only-mode -1)
     (erase-buffer)
-    (let ((updates nil))
+    (let ((updates nil)
+          (error-repos nil))
       (straight--map-repos
        (lambda (recipe)
          (straight--with-plist recipe (package local-repo)
            (let* ((default-directory (straight--repos-dir local-repo))
-                  (commits (straight--process-output "git" "log" "..@{u}" "--oneline")))
-             (unless (string-empty-p commits)
+                  (commits))
+             (condition-case nil
+                 (setq commits (straight--process-output "git" "log" "..@{u}" "--oneline"))
+               (error (push local-repo error-repos)))
+             (unless (or (string-empty-p commits)
+                         (not commits))
                (push (cons package (split-string commits "\n")) updates))))))
-      (insert (propertize "Recent Commits" 'face 'outline-1)
-              "\n\n")
-      (mapc (lambda (update)
-              (let* ((commits (cdr update))
-                     (package (car update))
-                     (dir (straight--repos-dir package)))
-                (insert
-                 (propertize
-                  (format "%s [%s commit%s]\n"
-                          package
-                          (number-to-string (length commits))
-                          (if (cdr commits) "s" ""))
-                  'face 'font-lock-constant-face))
-                (mapc (lambda (commit)
-                        (let ((rev (string-limit commit 7)))
-                          (insert "  ")
-                          (insert-text-button
-                           commit
-                           'follow-link t
-                           'face 'default
-                           'action (lambda (_)
-                                     (let ((default-directory dir))
-                                       (magit-show-commit (magit-commit-p rev)))))
-                          (newline)))
-                      (cdr update)))
-              (newline))
-            (cl-sort updates #'string< :key #'car))
+      (if updates
+          (progn
+            (insert (propertize "Recently Fetched Commits" 'face 'outline-1)
+                    "\n\n")
+            (mapc (lambda (update)
+                    (let* ((commits (cdr update))
+                           (package (car update))
+                           (dir (straight--repos-dir package)))
+                      (insert
+                       (propertize
+                        (format "%s [%s commit%s]\n"
+                                package
+                                (number-to-string (length commits))
+                                (if (cdr commits) "s" ""))
+                        'face 'font-lock-constant-face))
+                      (mapc (lambda (commit)
+                              (let ((rev (string-limit commit 7)))
+                                (insert-text-button
+                                 (concat "  " 
+                                         commit)
+                                 'follow-link t
+                                 'face 'default
+                                 'action (lambda (_)
+                                           (let ((default-directory dir))
+                                             (magit-show-commit (magit-commit-p rev)))))
+                                (newline)))
+                            (cdr update)))
+                    (newline))
+                  (cl-sort updates #'string< :key #'car)))
+        (insert (propertize "No Recently Fetched Commits" 'face 'outline-1)))
+      (when error-repos
+        (insert (propertize "Repos With No Upstream Branch" 'face 'outline-1) "\n")
+        (mapc (lambda (repo)
+                (insert "Click to set upstream: ")
+                (insert-text-button
+                 (file-name-base repo)
+                 'follow-link t
+                 'face 'default
+                 'action (lambda (_)
+                           (let ((default-directory repo)
+                                 (branch (magit-get-current-branch)))
+                             (magit-status repo)
+                             (magit-branch-configure branch)))))
+              error-repos))
       (special-mode)
       (pop-to-buffer (current-buffer))
       (goto-char (point-min)))))
