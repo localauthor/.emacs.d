476005c472c4cd13e1538fcfe33c9b001cf493a3
Grant Rosson <grantrosson@gmail.com>
2022-11-04 13:21:16 +0200

    Update elfeed for doi discovery

--- my-lisp/elfeed-setup.el
+++ my-lisp/elfeed-setup.el
@@ -6,23 +6,21 @@
         ("r" . gr/elfeed-mark-as-read)
         ("g" . elfeed-search-update--force)
         ("m" . elfeed-toggle-star)
+        ("d" . elfeed-doi-scihub-or-ebib)
         ("e" . gr/elfeed-show-eww)
         ("M-v" . golden-ratio-scroll-screen-down)
         ("b" . gr/elfeed-show-xwidget))
   (:map elfeed-search-mode-map
         ("RET" . gr/elfeed-search-show-entry)
         ("q" . bjm/elfeed-save-db-and-bury)
-        ("Q" . bjm/elfeed-save-db-and-bury)
         ("m" . elfeed-toggle-star)
-        ("M" . elfeed-toggle-star)
-        ("j" . mz/make-and-run-elfeed-hydra)
-        ("J" . mz/make-and-run-elfeed-hydra)
-        ("n" . (lambda () (interactive) (next-line) (call-interactively 'gr/elfeed-search-show-entry)))
-        ("p" . (lambda () (interactive) (previous-line) (call-interactively 'gr/elfeed-search-show-entry)))
-        ("b" . (lambda () (interactive) (call-interactively 'gr/elfeed-search-show-entry) (gr/elfeed-show-xwwp))))
+        ;;("n" . (lambda () (interactive) (next-line) (call-interactively 'gr/elfeed-search-show-entry)))
+        ;;("p" . (lambda () (interactive) (previous-line) (call-interactively 'gr/elfeed-search-show-entry)))
+        ("b" . (lambda () (interactive) (call-interactively 'gr/elfeed-search-show-entry) (gr/elfeed-show-xwidget)))
+        ("j" . hydra-elfeed/body))
   :config
   (setq elfeed-db-directory "~/Dropbox/.elfeeddb")
-  (setq elfeed-search-filter "@6-months-ago +unread +AmLit")
+  (setq elfeed-search-filter "+unread +AmLit")
   (setq elfeed-search-remain-on-entry t)
   (setq elfeed-show-entry-switch 'display-buffer)
 
@@ -50,50 +48,20 @@
     (elfeed-db-save)
     (quit-window))
 
-  ;; elfeed tags hydra
-
-  (defun z/hasCap (s) ""
-         (let ((case-fold-search nil))
-           (string-match-p "[[:upper:]]" s)
-           ))
-
-  (defun z/get-hydra-option-key (s)
-    "Return single upper case letter (converted to lower) or first."
-    (interactive)
-    (let ( (loc (z/hasCap s)))
-      (if loc
-          (downcase (substring s loc (+ loc 1)))
-        (substring s 0 1)
-        )))
-
-  (defun mz/make-elfeed-cats (tags)
-    "Returns a list of lists. Each one is line for the hydra configuration in the form (c function hint)."
-    (interactive)
-    (mapcar (lambda (tag)
-              (let* (
-                     (tagstring (symbol-name tag))
-                     (c (z/get-hydra-option-key tagstring))
-                     )
-                (list c (append '(elfeed-search-set-filter) (list (format "@6-months-ago +%s" tagstring) ))tagstring  )))
-            tags))
-
-  (defmacro mz/make-elfeed-hydra ()
-    `(defhydra mz/hydra-elfeed (:color blue)
-       ""
-       ,@(mz/make-elfeed-cats (elfeed-db-get-all-tags))
-       ("*" (elfeed-search-set-filter "@6-months-ago +star") "Starred")
-       ;;       ("M" elfeed-toggle-star "Mark")
-       ("A" (elfeed-search-set-filter "@6-months-ago") "All")
-       ("T" (elfeed-search-set-filter "@1-day-ago") "Today")
-       ("Q" bjm/elfeed-save-db-and-bury "Quit Elfeed" :color blue)
-       ("q" nil "quit" :color blue)
-       ))
-
-  (defun mz/make-and-run-elfeed-hydra ()
-    ""
-    (interactive)
-    (mz/make-elfeed-hydra)
-    (mz/hydra-elfeed/body))
+  (defhydra hydra-elfeed (:hint nil
+                                :color blue)
+    "
+_a_: AmLit  _j_: Journals  _l_: lit  _g_: geog  _k_: knowledge  _s_: starred"
+    ("a" (elfeed-search-set-filter "+AmLit"))
+    ("j" (elfeed-search-set-filter "+journal"))
+    ("l" (elfeed-search-set-filter "+lit"))
+    ("g" (elfeed-search-set-filter "+geography"))
+    ("k" (elfeed-search-set-filter "+knowledge"))
+    ("s" (elfeed-search-set-filter "+star"))
+    ("A" (elfeed-search-set-filter ""))
+    ("T" (elfeed-search-set-filter "@1-day-ago"))
+    ("Q" bjm/elfeed-save-db-and-bury :color blue)
+    ("q" nil :color blue))
   )
 
 (defun gr/elfeed-show-eww ()
@@ -148,12 +116,13 @@
 (defun gr/elfeed-search-show-entry (entry)
   "Display the currently selected item in a buffer."
   (interactive (list (elfeed-search-selected :ignore-region)))
-  ;;  (require 'elfeed-show)
-  (when (elfeed-entry-p entry)
-    ;;(elfeed-untag entry 'unread)
-    (elfeed-search-update-entry entry)
-    (unless elfeed-search-remain-on-entry (forward-line))
-    (elfeed-show-entry entry)))
+  (let ((switch-to-buffer-obey-display-actions nil))
+    ;;  (require 'elfeed-show)
+    (when (elfeed-entry-p entry)
+      ;;(elfeed-untag entry 'unread)
+      (elfeed-search-update-entry entry)
+      (unless elfeed-search-remain-on-entry (forward-line))
+      (elfeed-show-entry entry))))
 
 ;;;###autoload
 (defun gr/elfeed-open-new-window ()
@@ -163,15 +132,15 @@
 
 ;;;###autoload
 (defun gr/elfeed-open ()
-(interactive)
-(let ((inhibit-message t))
-  (elfeed)
-  (set-frame-size (selected-frame) 150 46)
-  (delete-other-windows)
-  (if (bound-and-true-p truncate-lines)
-      (elfeed-update)
-    (toggle-truncate-lines))
-  (elfeed-update)))
+  (interactive)
+  (let ((inhibit-message t))
+    (elfeed)
+    (set-frame-size (selected-frame) 150 46)
+    (delete-other-windows)
+    (if (bound-and-true-p truncate-lines)
+        (elfeed-update)
+      (toggle-truncate-lines))
+    (elfeed-update)))
 
 
 (use-package elfeed-summary
@@ -193,4 +162,115 @@
                  (:elements
                   (query . (emacs)))))))
 
+(defcustom elfeed-doi-regexps
+  '("scheme=\"doi\" content=\"\\([^\"]*\\)\""
+    "citation_doi\" content=\"\\([^\"]*\\)\""
+    "data-doi=\"\\([^\"]*\\)\""
+    "content=\"\\([^\"]*\\)\" name=\"citation_doi"
+    "objectDOI\" : \"\\([^\"]*\\)\""
+    "doi = '\\([^']*\\)'"
+    "\"http://dx.doi.org/\\([^\"]*\\)\""
+    "/doi/\\([^\"]*\\)\">"
+    "doi/full/\\(.*\\)&"
+    "doi=\\([^&]*\\)&amp")
+  "List of regexps to match a DOI.
+The doi should be in group 1 so that (match-string 1) contains
+the DOI.")
+
+(defun elfeed-doi-scihub-or-ebib ()
+  "Get article for elfeed entry or add to ebib."
+  (interactive)
+  (let* ((url (elfeed-entry-link elfeed-show-entry))
+	 (content (elfeed-deref (elfeed-entry-content elfeed-show-entry)))
+         (doi)
+         (dois))
+    (if (string-match "DOI: \\(.*\\)$" content)
+	(setq doi (match-string 1))
+      (setq dois (with-current-buffer (url-retrieve-synchronously url)
+                   (cl-loop for doi-pattern in elfeed-doi-regexps
+	                    do
+	                    (goto-char (point-min))
+	                    (while (re-search-forward doi-pattern nil t)
+		              (cl-pushnew (match-string 1) dois :test #'equal)))
+                   (reverse dois)))
+      (cond
+       ((= 1 (length dois))
+        (setq doi (car dois)))
+       ((> (length dois) 1)
+        (setq doi (completing-read
+                   "Select a DOI: "
+                   (let ((dois '()))
+		     (with-current-buffer (url-retrieve-synchronously url)
+		       (loop for doi-pattern in elfeed-doi-regexps
+			     do
+			     (goto-char (point-min))
+			     (while (re-search-forward doi-pattern nil t)
+			       (push
+				;; Cut off the doi, sometimes
+				;; false matches are long.
+				(cons (format "%40s | %s"
+					      (substring
+					       (match-string 1)
+					       0 (min
+						  (length (match-string 1))
+						  40))
+					      doi-pattern)
+				      (match-string 1))
+				dois)))
+		       (reverse dois))))))
+       (t (error "No DOI found"))))
+    (let* ((select (completing-read "Select: "
+                                    '("Scihub Lookup"
+                                      "Add to Ebib"))))
+      (cond
+       ((equal select "Scihub Lookup")
+        (scihub doi))
+       ((equal select "Add to Ebib")
+        (ebib-zotero-import-identifier doi))))))
+
 (provide 'elfeed-setup)
+
+
+;; elfeed tags hydra
+;; (defun z/hasCap (s) ""
+;;        (let ((case-fold-search nil))
+;;          (string-match-p "[[:upper:]]" s)
+;;          ))
+
+;; (defun z/get-hydra-option-key (s)
+;;   "Return single upper case letter (converted to lower) or first."
+;;   (interactive)
+;;   (let ((loc (z/hasCap s)))
+;;     (if loc
+;;         (downcase (substring s loc (+ loc 1)))
+;;       (substring s 0 1))))
+
+;; (defun mz/make-elfeed-cats (tags)
+;;   "Returns a list of lists. Each one is line for the hydra configuration in the form (c function hint)."
+;;   (interactive)
+;;   (mapcar (lambda (tag)
+;;             (let* ((tagstring (symbol-name tag))
+;;                    (c (z/get-hydra-option-key tagstring)))
+;;               (list c (append
+;;                        '(elfeed-search-set-filter)
+;;                        (list (format "@6-months-ago +%s" tagstring)))
+;;                     tagstring)))
+;;           tags))
+
+;; (defmacro mz/make-elfeed-hydra ()
+;;   `(defhydra mz/hydra-elfeed (:color blue)
+;;      ""
+;;      ,@(mz/make-elfeed-cats (elfeed-db-get-all-tags))
+;;      ("*" (elfeed-search-set-filter "@6-months-ago +star") "Starred")
+;;      ;;       ("M" elfeed-toggle-star "Mark")
+;;      ("A" (elfeed-search-set-filter "@6-months-ago") "All")
+;;      ("T" (elfeed-search-set-filter "@1-day-ago") "Today")
+;;      ("Q" bjm/elfeed-save-db-and-bury "Quit Elfeed" :color blue)
+;;      ("q" nil "quit" :color blue)
+;;      ))
+
+;; (defun mz/make-and-run-elfeed-hydra ()
+;;   ""
+;;   (interactive)
+;;   (mz/make-elfeed-hydra)
+;;   (mz/hydra-elfeed/body))
