ec7f578fa90c9fa3c84058d47fcff25f6a5cce2f
Grant Rosson <grantrosson@gmail.com>
2023-09-05 23:00:23 +0300

    Update devonthink-dir.el

--- my-lisp/devonthink-dir.el
+++ my-lisp/devonthink-dir.el
@@ -4,27 +4,9 @@
 
 ;;; Code:
 
-;; (require 'citar)
-;; (require 'consult)
-;; (require 'org)
+(defvar devonthink-dir "~/Databases/Academic.dtBase2/Files.noindex/")
 
-(defvar devonthink-dir)
-
-(setq devonthink-dir "~/Databases/Academic.dtBase2/Files.noindex/")
-
-(defun devonthink-set-dir ()
-  "Set DEVONthink DB."
-  (interactive)
-  (let ((db (completing-read "Choose: " '(Academic Personal) nil t)))
-    (setq devonthink-dir (format "~/Databases/%s.dtBase2/Files.noindex/" db))))
-
-;; (defun devonthink-dir-find-file (key-entry &optional initial)
-;;   "Search devonthink archive for file, using `consult-find'."
-;;   (interactive (list (citar-select-ref)))
-;;   (let ((key (car key-entry)))
-;;     (if key
-;;         (consult-find devonthink-dir (format "%s" key))
-;;       (consult-find devonthink-dir initial))))
+(defvar devonthink-pdf-dir "~/Databases/Academic.dtBase2/Files.noindex/pdf/")
 
 (defun devonthink-dir-find-file (&optional initial)
   "Search devonthink archive for file, using `consult-find'."
@@ -32,6 +14,19 @@
   (when (equal current-prefix-arg '(4))
     (devonthink-set-dir))
   (consult-find devonthink-dir initial))
+
+(defun devonthink-set-dir ()
+  "Set DEVONthink DB."
+  (interactive)
+  (let ((db (completing-read "Choose: " '(Academic Personal) nil t)))
+    (setq devonthink-dir (format "~/Databases/%s.dtBase2/Files.noindex/" db))))
+
+(defun consult-ripgrep-devonthink ()
+  (interactive)
+  (let ((consult-ripgrep-args "rga --null --line-buffered --color=never --max-columns=1000 --path-separator /   --smart-case --no-heading --line-number .")
+        (vertico-count 15)
+        (consult-preview-key nil))
+    (consult-ripgrep devonthink-pdf-dir)))
 
 (defun org-devonthink-setup ()
   "Register org-devonthink link type"
@@ -42,24 +37,6 @@
 (defun org-devonthink-item-open (uid)
   "Open the given UID, which is a reference to an item in Devonthink."
   (shell-command (concat "open \"x-devonthink-item:" uid "\"")))
-
-;; (defun gr/devonthink-find-file (keys-entries)
-;;   "Search devonthink archive for file, using `consult-find'."
-;;   (interactive (list (citar-select-refs :rebuild-cache current-prefix-arg)))
-;;   (kill-new (or (car (citar--extract-keys keys-entries)) ""))
-;;   (minibuffer-with-setup-hook
-;;       'yank
-;;     (define-key embark-file-map (kbd "RET") 'find-file)
-;;     (consult-find gr/devonthink-dir)))
-
-
-(defun consult-ripgrep-devonthink ()
-  (interactive)
-  (let ((consult-ripgrep-args "rga --null --line-buffered --color=never --max-columns=1000 --path-separator /   --smart-case --no-heading --line-number .")
-        (vertico-count 15)
-        (consult-preview-key nil))
-    (consult-ripgrep gr/pdf-directory)))
-
 
 (provide 'devonthink-dir)
 ;;; dir-devonthink.el ends here
